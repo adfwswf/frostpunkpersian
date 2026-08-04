37f2ec572680d1161d5fa9603e3b9784fb30edf8
ee <tiger.sabri31300@gmail.com>
2026-08-04 23:56:10 +0330

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,16 +1,31 @@
-const CACHE_NAME = 'last-warmth-v3';
-const urlsToCache = ['./', './index.html', './icon.png'];
+const CACHE_NAME = 'last-warmth-v4';
+const urlsToCache = [
+  './',
+  './index.html',
+  './style.css',
+  './game.js',
+  './icon.png',
+  './manifest.json'
+];
 
 self.addEventListener('install', event => {
-  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
   self.skipWaiting();
 });
 
 self.addEventListener('activate', event => {
-  event.waitUntil(caches.keys().then(keys => Promise.all(keys.map(key => key !== CACHE_NAME ? caches.delete(key) : null))));
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(key => key !== CACHE_NAME ? caches.delete(key) : null)))
+  );
   self.clients.claim();
 });
 
 self.addEventListener('fetch', event => {
-  event.respondWith(caches.match(event.request).then(response => response || fetch(event.request)));
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
 });
